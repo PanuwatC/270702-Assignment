5c4aa7aaaf8fa7ba18b4c02587697c3167bbe0cc
PanuwatC <panuwat.chinasri@gmail.com>
2022-07-24 22:46:33 +0700

    update to play with python
    update code change from only player to play with python

--- 270702_Assign2.py
+++ 270702_Assign2.py
@@ -1,9 +1,15 @@
+import random
 N = int(input("How many stick in the pile : "))
 print("There are",N,"sticks in the pile.")
 name = str(input("What is your name : "))
-count = 0
+
+i = 0
 while(N != 0):
-  x = int(input(str(name) + ", how many stick you will take? (1 or 2): "))
+  if(i%2 == 0):
+    x = int(input(str(name) + ", how many stick you will take? (1 or 2): "))
+  else:
+    x = random.randint(1,2)
+    print("Computer, takes: ",x)
   if(x > 2):
     print("No you cannot take more than 2 stick!")
   elif(x < 1):
@@ -12,10 +18,15 @@ while(N != 0):
     print("There are no enough sticks to take.")
   else:
     N = N-x
-    count += 1 
     if(N == 0):
-      print("OK. There is no stick left in the pile. You spent",count,"times.")
-    elif(N):
-      print("There are",N,"sticks in the pile.")
-      
+      if(i%2 == 0):
+        print(name,end=", take the last stick.\n")
+        print("Computer win !!!")
+      else:
+        print("Computer, take the last stick.")
+        print(name,"win (Computer, am sad T_T)")
+    else:
+      print("There are",N,"sticks in the pile.\n")
+    i += 1 
+
       
