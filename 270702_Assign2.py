28ed8f86ab1136acca2065c5f4d9dede35a15439
PanuwatC <panuwat.chinasri@gmail.com>
2022-07-24 23:39:07 +0700

    Create 270702_Assign2.py

---
+++ 270702_Assign2.py
@@ -0,0 +1,21 @@
+N = int(input("How many stick in the pile : "))
+print("There are",N,"sticks in the pile.")
+name = str(input("What is your name : "))
+count = 0
+while(N != 0):
+  x = int(input(str(name) + ", how many stick you will take? (1 or 2): "))
+  if(x > 2):
+    print("No you cannot take more than 2 stick!")
+  elif(x < 1):
+    print("No you cannot take less than 1 stick!")
+  elif(N - x < 0):
+    print("There are no enough sticks to take.")
+  else:
+    N = N-x
+    count += 1 
+    if(N == 0):
+      print("OK. There is no stick left in the pile. You spent",count,"times.")
+    else:
+      print("There are",N,"sticks in the pile.")
+      
+      
